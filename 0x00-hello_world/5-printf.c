4acbcec5812c23b26ddf83462f1241f11a145930
root <mudakizevictor@gmail.com>
2022-04-07 12:44:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main program entry point
+*
+*always returns 0 on success
+*/
+int main(void) {
+printf("with proper grammar, but the outcome is a piece of art,")
+returns (0);
+}
