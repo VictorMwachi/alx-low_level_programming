749cf73ce01e2b792c468fd61468afb6ea7b50e2
root <mudakizevictor@gmail.com>
2022-04-07 12:26:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <string.h>
-/* program entry poin*/
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ * 
+ */
 
 int main(void)
 {
-
-char str1[] = "\"Programming is like building a multilingual puzzle";
-puts(str1);
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
