351ef56bda6be9ac3a3f149c067e3cca49402c74
root <mudakizevictor@gmail.com>
2022-04-07 12:54:05 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
 *
 *always returns 0 on success
 */
-int main(void) {
-printf("with proper grammar, but the outcome is a piece of art,");
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,/n");
 return (0);
 }
