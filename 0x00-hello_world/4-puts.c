79132650c927159f05d2ab96cd9a0da24f8df473
root <mudakizevictor@gmail.com>
2022-04-07 12:17:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+/* program entry poin*/
 
 int main(void)
 {
 
-char str1[] = '"Programming is like building a multilingual puzzle\n';
+char str1[] = "\"Programming is like building a multilingual puzzle\n";
 puts(str1);
 
 return (0);
