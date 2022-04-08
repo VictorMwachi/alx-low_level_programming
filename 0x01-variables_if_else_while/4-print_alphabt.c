d9922edeefc632ad2fa6319be6f68452f47e64d4
root <mudakizevictor@gmail.com>
2022-04-08 13:13:32 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,10 +5,11 @@
  * Return:always o (success)
  */
 int main(void)
-{char ch = a;
+{char ch = 'a';
 while (ch <= 'z')
 {if (ch != 'q' && ch != 'e')
 {putchar(ch); }
+ch++;
 }
 putchar('\n');
 return 0;
