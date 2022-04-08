3d714d4ff0a7a8ca3d75d56fd84b539da62a9ce6
root <mudakizevictor@gmail.com>
2022-04-08 14:06:14 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Return:aleays 0 (success)
+ */
+int main(void)
+{char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{putchar(ch); }
+putchar('\n');
+return (0);
+}
