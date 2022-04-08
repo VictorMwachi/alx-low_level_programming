3c005450c2307d959cd4f65576a379030997323e
root <mudakizevictor@gmail.com>
2022-04-08 12:30:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Return:always o (success)
+ */
+int main(void)
+{
+char lower
+for (lower='a';lower <= 'z'; lower++;)
+{
+putchar(lower);
+}
+putchar('\n');
+return (0);
+}
