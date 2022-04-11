a82a13be2eea475b547a989be49b3364eae2a8da
root <mudakizevictor@gmail.com>
2022-04-11 12:15:33 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,11 +5,11 @@
  * Return: always 0 success
  */
 int main(void)
-{int a=48;
-int b=48;
+{int a = 48;
+int b = 48;
 for (a = 48; a <=57; a++)
 {for (b = 48; b <= 57; b++)
-{if (a!= b && a < b)
+{if (a != b && a < b)
 {putchar(a);
 putchar(b);
 if(a == 56 && b == 57)
