437634e65ed2429d6499e9df77fc1dab39729e64
root <mudakizevictor@gmail.com>
2022-04-11 05:06:11 -0700

    101-quote.c

---
+++ 0x01-variables_if_else_while/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return:always 0 success 1 error
+ */
+int main(void)
+{write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1); }
