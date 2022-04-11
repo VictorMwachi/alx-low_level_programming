2492532d2c4a4b7ecfe682d7d9971f27c6589d82
root <mudakizevictor@gmail.com>
2022-04-11 12:40:08 -0700

    101-print_comb.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{int a = 48;
+int b = 48;
+int c = 48;
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+for (c = 48; c<= 57; c++)
+{
+if (a != b && a != c && b != c && a < b && b < c)
+{putchar(a);
+putchar(b);
+putchar(c);
+if (a == 55 && b == 56 && c == 57)
+{break; }
+putchar(',');
+putchar(' '); } } } }
+putchar('\n');
+return (0); }
