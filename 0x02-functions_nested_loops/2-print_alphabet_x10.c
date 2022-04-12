7b35a3767deb90eeae31068cbdf4b008f21eab15
root <mudakizevictor@gmail.com>
2022-04-12 10:56:53 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet_x10-prints lower case letters 1times
+ */
+void print_alphabet_x10(void)
+{char ch = a;
+int i =0;
+while (i < 10)
+{while (ch <= 'z')
+{_putchar(ch);
+ch++;
+} } }
