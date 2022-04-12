287433d16ee9c696cdd535684ff648c4948dafcb
root <mudakizevictor@gmail.com>
2022-04-12 11:08:55 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,9 @@ void print_alphabet_x10(void)
 {char ch = 'a';
 int i = 0;
 while (i < 10)
-{while (ch <= 'z')
-{_putchar(ch);
-ch++;
-} _putchar('\n');} }
+{
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++; }
+_putchar('\n');} }
