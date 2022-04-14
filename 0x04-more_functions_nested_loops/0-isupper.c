185e2b6c871b1da7bbe43acd3febf3265df70026
root <mudakizevictor@gmail.com>
2022-04-14 10:59:40 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <ctype.h>
 /**
  * _isupper-checks for upper case character
  */
 int _isupper(int c)
-{if (c >= 'A'&& c <= 'Z')
+{
+if (c >= 'A'&& c <= 'Z')
 {return (1); }
 else
 {return (0); } }
