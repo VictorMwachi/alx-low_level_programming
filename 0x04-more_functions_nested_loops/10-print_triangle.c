deac981f44fdc1d91fe380d830795bb774a8e7fe
root <mudakizevictor@gmail.com>
2022-04-16 13:35:19 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_triangle()-prints tringle the sze specified
+ *
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{int i,s = size - 1,k;
+if (size > 0)
+{
+for(i = 0; i < size; i++)
+{
+	for (k = 0; k < size; k++)
+	{
+		if (k < n)
+		{_putchar(' ');}
+		else
+		{_putchar('#');} }
+	n--;
+	_putchar('\n'); } }}
