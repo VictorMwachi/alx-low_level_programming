73b1c7bc07a338111f3ebbca03afd0c65446bb8c
root <mudakizevictor@gmail.com>
2022-04-20 11:30:43 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,9 +8,10 @@
 void rev_string(char *s)
 {
 	int i, len, temp;
+
 	len = strlen(s); /* use strlen() to get the length of str string  */
 	/* use for loop to iterate the string*/
-	for (i = 0; i < len/2; i++)
+	for (i = 0; i < len / 2; i++)
 	{/* temp variable use to temporary hold the string  */
 		temp = s[i];
 		s[i] = s[len - i - 1];
