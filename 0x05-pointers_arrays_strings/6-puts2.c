17d10f0342ebb3d30e67ade6febd8c9c175b4a4c
root <mudakizevictor@gmail.com>
2022-04-20 12:03:00 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints any other charactor starting from first
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	int i = 0; len = strlen(str);
+
+	for (i = 0; i <= len - 1; i++)
+	{
+		while (i % 2 == 0)
+		{
+			if (str[i] != '\0')
+			{putchar(str[i]); }
+		}
+	}
+}
