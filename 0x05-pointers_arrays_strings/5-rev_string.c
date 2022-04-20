13128e57fb9a0256ef617a59b9c972e6cefdf371
root <mudakizevictor@gmail.com>
2022-04-20 11:27:46 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * print_rev - prints string in reverse
+ * rev_string - prints string in reverse
  * @s: string to be printed
  */
 void rev_string(char *s)
 {
 	int i, len, temp;
 	len = strlen(s); /* use strlen() to get the length of str string  */
-		     /* use for loop to iterate the string*/
-		    for (i = 0; i < len/2; i++)
-		    {  
-		    /* temp variable use to temporary hold the string  */
-		    temp = s[i];  
-		    s[i] = s[len - i - 1];  
-		    s[len - i - 1] = temp;  
-		    }}
+	/* use for loop to iterate the string*/
+	for (i = 0; i < len/2; i++)
+	{/* temp variable use to temporary hold the string  */
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
