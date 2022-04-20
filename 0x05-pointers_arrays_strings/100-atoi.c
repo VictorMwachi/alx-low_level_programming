d280f744f6962bde61c23ab3693793c76b6d0ef9
root <mudakizevictor@gmail.com>
2022-04-20 15:23:32 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_atoi - convert string to integer
+ *@s:string to be converted to integer
+ */
+int _atoi(char *s)
+{
+	int b = int(s);
+	return (0);
+}
