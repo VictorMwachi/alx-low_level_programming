93d6f852484fb77d38663ffb48337827450583ba
root <mudakizevictor@gmail.com>
2022-04-20 13:42:01 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 {
 	int i, len = strlen(str), half = (len - 1)/2;
 
-	for (i = half; i <= half + half + 1; i++)
+	for (i = half; i <= half + half; i++)
 	{
 		if (i != '\0')
 			putchar(str[i]);
