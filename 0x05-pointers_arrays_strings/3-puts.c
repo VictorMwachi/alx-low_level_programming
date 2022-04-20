df3428cf34c078bcb7fcd10a4c143b56960d839d
root <mudakizevictor@gmail.com>
2022-04-20 04:43:13 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts-prints a string followed by a new line
+ * @str: string
+ */
+void _puts(char *str)
+{
+	puts(str);
+	puts("\n");
+}
