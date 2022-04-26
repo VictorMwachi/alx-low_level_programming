ecfa708abb4a4b49d7257e4e9528e7e00d0c4ea8
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-04-26 15:19:34 +0300

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
- * _strchr -  locates a character in a string
- * @s: string of characters
- * @c: character to ve located in string s
- * Return: Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+ * _strchr - locates char in string
+ * @s: pointer to string
+ * @c: char to locate
+ *
+ * Return: pointer to first occurence of c in s
  */
 char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 		s++;
 	}
 	return (!c ? s : NULL);
