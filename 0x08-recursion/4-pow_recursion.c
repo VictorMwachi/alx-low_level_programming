10fb9ff7e4471ddc40551caa4ecefdc9f4c28f68
root <mudakizevictor@gmail.com>
2022-04-29 14:11:50 -0700

    3-factorial.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _pow_recursion - gets power of integer x bi recursion
+ * @x: integer to be raised to power y
+ * @y: power of x
+ * Return: returns the answer
+ */
+int _pow_recursion(int x, int y)
+{
+	int r;
+	
+	if (y > 0)
+	{
+		r = x*x;
+		y--;
+		_pow_recursion(x, y);
+	}
+	else if (y < 0)
+	{
+		r = -1;
+	}
+	else
+	{
+		r = 1;
+	}
+	return (r);}
