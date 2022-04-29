97b4e7ea4b38e3f2fbd66b71d0d097b0ffbb3d8d
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-04-30 01:02:37 +0300

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,26 +1,18 @@
 #include "main.h"
+#include <stddef.h>
+
 /**
- * _pow_recursion - gets power of integer x bi recursion
- * @x: integer to be raised to power y
- * @y: power of x
- * Return: returns the answer
+ * _pow_recursion - returns the x to the power y
+ * @x: the base
+ * @y: the exponent
+ *
+ * Return: the integer result, -1 on error
  */
 int _pow_recursion(int x, int y)
 {
-	int r;
-	
+	if (y < 0)
+		return (-1);
 	if (y > 0)
-	{
-		r =x * _pow_recursion(x, y);
-		y--;
-		_pow_recursion(x, y);
-	}
-	else if (y < 0)
-	{
-		r = -1;
-	}
-	else
-	{
-		r = 1;
-	}
-	return (r);}
+		return (x * _pow_recursion(x, y - 1));
+	return (1);
+}
