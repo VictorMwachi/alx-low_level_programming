42926b9be5bda9fc91ad5f77999adb0f80da9c3f
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-06 15:15:58 +0300

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,13 +11,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *p = (char*)malloc(sizeof(char) * size);
+	char *arr = (char*)malloc(sizeof(char) * size);
 
-	if (size == 0 || p == NULL)
+	if (size == 0 || arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
-		p[i] = c;
+		arr[i] = c;
 	}
-	return (p);
+	return (arr);
 }
