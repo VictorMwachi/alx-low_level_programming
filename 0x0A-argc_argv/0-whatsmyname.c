50c134ade5e1d8c49d0967c6217f551f833bf88a
root <mudakizevictor@gmail.com>
2022-05-07 00:49:05 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * @argc: size of agg
+ * @argv: array of argurments
+ * Return: always (0) success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+}
