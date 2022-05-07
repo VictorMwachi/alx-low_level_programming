6488a495582381f13071f15d837f6259d2e879e6
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-07 12:18:33 +0300

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- function that returns the product of 2 numbs
+ * @argc: counts the number of args passed
+ * @argv: an array of strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, product = 1;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			product *= atoi(argv[i]);
+		}
+		printf("%d\n", product);
+	}
+	exit(EXIT_SUCCESS);
+}
