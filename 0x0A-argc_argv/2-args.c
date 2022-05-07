b37cef43ae76384d48185ed83415c81dc0434477
root <mudakizevictor@gmail.com>
2022-05-07 02:08:33 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,14 +9,14 @@
 int main(int argc, char *argv[])
 {
 	int i = 0;
-
+	
 	if (argc > 0)
-  {
-    while (i < argc)
-    {
-      printf("%s\n", argv[i]);
-      i++;
-    }
-  }
+	{
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
+	}
 	return (0);
 }
