ffa02f961e0178555d5e568003757a79d9cf3daa
root <mudakizevictor@gmail.com>
2022-05-13 01:29:16 -0700

    main.c

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - programs entry point
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
