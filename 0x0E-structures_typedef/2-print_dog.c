840174cb9a8ca4d3be619cac674718ac846995f1
root <mudakizevictor@gmail.com>
2022-05-13 10:52:04 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,6 +4,7 @@
 /**
  * print_dog - prints struct dog
  * @d: pointer to dtruct dog
+ * Return: nothing
  */
 void print_dog(struct dog *d)
 {
@@ -13,16 +14,16 @@ void print_dog(struct dog *d)
 	{
 		if (d->name != NULL)
 		{
-			printf("%s\n", d->name);
+			printf("%s\n", d -> name);
 		}
 		else
 		{
 			printf("Name: (nil)\n");
 		}
-		printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d -> age);
 		if (d->owner != NULL)
 		{
-			printf("%s\n", d->owner);
+			printf("%s\n", d -> owner);
 		}
 		else
 		{
