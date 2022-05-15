31ef532e67e10fc3f66fd594c8ff37b1dba184d7
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-15 23:00:20 +0300

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(sizeof(nmemb) * size);
+	p = malloc(sizeof(nmemb * size));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
