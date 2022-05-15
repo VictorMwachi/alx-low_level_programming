d99c1aabad123ea00d713a12019ff46ec552ed75
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-15 22:21:33 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k, m, a;
 
 	i = j = a = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i] != '\0')
 	{
 		i++;
