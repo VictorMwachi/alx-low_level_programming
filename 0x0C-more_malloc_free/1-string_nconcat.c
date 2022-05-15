b4bfcdff1004e454018290165ba5ac8476893d1e
root <mudakizevictor@gmail.com>
2022-05-15 12:01:46 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,11 +13,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k, m, a;
 
 	i = j = a = 0;
-	while (s[i] != '\0')
+	while (s1[i] != '\0')
 	{
 		i++;
 	}
-	while (s[j] != '\0')
+	while (s2[j] != '\0')
 	{
 		j++;
 	}
