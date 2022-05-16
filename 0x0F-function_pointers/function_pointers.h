34a434a87acbb84b8e7810864a87b5ab9ea54bf7
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-16 21:55:56 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
 #ifndef FUNCTION_POINTER
-#define FUNC6TION_POINTER
+#define FUNCTION_POINTER
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
