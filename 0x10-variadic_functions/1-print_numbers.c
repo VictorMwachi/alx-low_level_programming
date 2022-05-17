34ac844f0314580065fc2aba3542966bd869d674
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-17 14:47:03 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,7 +10,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	int i;
 
-	va_start(n, ap);
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		if (separator != NULL)
