5fa152b92fef9b349b719ac4df2368ce5738e97c
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-18 23:11:33 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include "calc.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - program entry point
  * @argc: no of argurments passed
@@ -8,4 +9,13 @@
  */
 int main (int argc, char *argv[])
 {
-	if
+	if (argc < 4)
+	{
+		return (-1);
+	}
+	if (argv[2] == "+" || argv[2] == "-" || argv[2] == "/" || argv[2] == "%" || argv[2] == "*")
+	{
+		printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+	}
+	return (0);
+}
