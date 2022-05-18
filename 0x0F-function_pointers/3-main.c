4035a2352ca922f86ebaeba4d32e2040b62e13cc
root <mudakizevictor@gmail.com>
2022-05-18 13:00:09 -0700

    main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,11 @@
+#include "calc.h"
+#include <stdio.h>
+/**
+ * main - program entry point
+ * @argc: no of argurments passed
+ * @argv: string of argurements
+ * Return: 0 always success
+ */
+int main (int argc, char *argv[])
+{
+	if
