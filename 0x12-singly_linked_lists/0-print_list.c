cf1acc6bce58e764588f9d3347bdf71eac8fcf0d
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-25 23:02:19 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -9,7 +9,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t n;
-	for(n = 0; *h; n++)
+	for(n = 0; h; n++)
 	{
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
