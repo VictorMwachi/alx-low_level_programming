83eb87d159a720755efb3419c877f0f9baf9b61d
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-26 20:06:01 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,12 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 	
-	if (h == NULL)
-		return (-1);
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		n++;
 	}
