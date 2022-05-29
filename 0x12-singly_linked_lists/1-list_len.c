9f760fca8f4adf08ca98739dd02c9a01deb54c26
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-29 23:52:39 +0300

    2-add_node.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
+#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * list_len - finds the number of nodes in a list
  * @h: singly linked list
@@ -8,11 +8,9 @@
  */
 size_t list_len(const list_t *h)
 {
-	unsigned int i;
-	const list_t *temp;
+	int i;
 
-	temp = h;
-	for (i = 0; temp; i++)
-		temp = temp->next;
+	for (i = 0; h; i++)
+		h = h->next;
 	return (i);
 }
