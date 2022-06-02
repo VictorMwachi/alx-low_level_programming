86312478d3cbfe750d6e860a60d50adbe7596f9c
root <mudakizevictor@gmail.com>
2022-06-02 13:02:06 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,14 +13,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	fd = open(filename | O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
 	rdf = read(fd, buf,letters);
-	wrtf = write(STDOUT_FILE_NO, buf, rdf);
+	wrtf = write(STDOUT_FILENO, buf, rdf);
 	close (fd);
 	free (buf);
 	return (wrtf);
