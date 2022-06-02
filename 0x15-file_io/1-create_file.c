8fc5ed5ac087d16934733e7e3f9bc8c67ed8ac8f
root <mudakizevictor@gmail.com>
2022-06-02 14:14:35 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	len = 0;
+	if (text_content)
+	{
 	while (*text_content++)
 		len++;
 	wrtf = write(fd, text_content, len);
@@ -28,5 +30,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(fd);
+	}
 	return (1);
 }
