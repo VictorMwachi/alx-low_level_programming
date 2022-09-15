d3a6c300594ff70629d7a31d4178d504a8580e15
root <mudakizevictor@gmail.com>
2022-09-15 14:28:15 -0700

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,13 @@
+"""perimetr module"""
+
+def island_perimeter(grid):
+
+    """returns the perimeter of an island"""
+    per=0
+    for row in grid:
+        for val in row:
+            per+=val
+    if per == 1:
+        return 4
+    else:
+        return ((per -2)*2)+6)
