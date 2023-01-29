02bc7fd71c2152d5d39b5d10999634beb934a254
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-29 16:53:49 +0300

    1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,31 @@
+ int linear_search(int *array, size_t size, int value)
+ {
+     size_t mid, L, i = 0;
+     size_t R = size - 1;
+     if (array == NULL && L > R)
+     {
+         return -1;
+     }
+     mid = (L + R) / 2;
+     if (value > array[mid]) {
+         for (i = mid + 1; i < R; i++) {
+             if (value == array[i]) {
+                 return i;
+             }
+         }
+     }
+     else if (value < array[mid]){
+         for(i = 0; i<= mid - 1; i++){
+             if(value == array[i]){
+                 return i;
+             }
+         }
+     }
+     else{
+         return mid;
+     }
+     
+     return -1;
+     
+     
+ }
