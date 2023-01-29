7602acd78dfc924132ff484d049d52ba6c2712c4
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-29 17:07:03 +0300

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 int linear_search(int *array, size_t size, int value)
  {
-     size_t mid, L, i = 0;
+     size_t mid, L = 0, i = 0;
      size_t R = size - 1;
      if (array == NULL && L > R)
      {
